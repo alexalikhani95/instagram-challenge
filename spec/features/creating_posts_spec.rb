330f1b62ec47a7426c1dcaf74abe34a3535e8e77
Alex <alikhanialex@gmail.com>
2019-02-04 10:31:40 +0000

    first test written

---
+++ spec/features/creating_posts_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper.rb'
+
+feature 'Creating posts' do
+  scenario 'can create a post' do
+    visit '/'
+    click_link 'New Post'
+    attach_file('Image', 'spec/files/images/fish.jpg')
+    fill_in 'Caption', with: 'Guppy #Fishtank'
+    click_button 'Create Post'
+    expect(page).to have_content('#Fishtank')
+    expect(page).to have_css("img[src*='fish.jpg']")
+  end
+end

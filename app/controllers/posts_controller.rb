49ee2bbb46303ad82e1de5cd8e412ed18bd6395f
Alex <alikhanialex@gmail.com>
2019-02-06 19:54:35 +0000

    bug fixed, user now can post a pic with a caption

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
+  # before_action :set_food, only: %i[show edit update destroy]
+
   def index; end
 
-  def new; end
-end
+  def new
+    p 'got here'
+    @post = Post.new
+  end
 
-def new
-  @post = Post.new
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+    end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
 end

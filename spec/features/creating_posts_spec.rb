7565fe51ce907e64940c457272b2c3c80459c65c
Alex <alikhanialex@gmail.com>
2019-02-07 09:00:52 +0000

    failing test written for image to be required to make a post

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -10,4 +10,14 @@ feature 'user makes posts' do
     expect(page).to have_content('#Fishtank')
     expect(page).to have_css("img[src*='fish.jpg']")
   end
+
+  feature 'needs an image to make a post' do
+    scenario 'image required for post' do
+      visit '/'
+      click_link 'New Post'
+      fill_in 'Caption', with: 'Tropical Fish'
+      click_button 'Create Post'
+      expect(page).to have_content('Image required to make a post!')
+    end
+  end
 end
